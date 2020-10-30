bccbbe8087a86769ae189dd4edbdc29b86c40678
PavelAlkhin <unicon2000@gmail.com>
2020-10-30 11:21:20 +0200

    Signed-off-by: PavelAlkhin <unicon2000@gmail.com>

--- 2020-08-otus-spring-alkhin/spring-02/src/main/java/ru/otus/spring/rest/BookControllerRest.java
+++ 2020-08-otus-spring-alkhin/spring-02/src/main/java/ru/otus/spring/rest/BookControllerRest.java
@@ -2,7 +2,9 @@ package ru.otus.spring.rest;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.otus.spring.models.Book;
 import ru.otus.spring.repositories.BookRepository;
 import ru.otus.spring.rest.dto.BookDto;
 import java.util.List;
@@ -15,8 +17,15 @@ public class BookControllerRest {
     private final BookRepository repBook;
 
     @GetMapping("/api/books")
-    public List<BookDto> getAllBooks() {
-        return repBook.findAll().stream().map(BookDto::toDto)
+    public List<BookDto> getAllBooks(@RequestParam(value = "id", required = false) String id) {
+
+        if(id == null) {
+            return repBook.findAll().stream().map(BookDto::toDto)
+                    .collect(Collectors.toList());
+        }
+
+        List<BookDto> bookDtoList =  repBook.findById(id).stream().map(BookDto::toDto)
                 .collect(Collectors.toList());
+        return bookDtoList;
     }
 }
