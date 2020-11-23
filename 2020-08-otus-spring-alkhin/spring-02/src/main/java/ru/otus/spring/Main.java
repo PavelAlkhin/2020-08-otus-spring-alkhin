5104c17f3d1787c397ff9cc83356c50b49c827a6
PavelAlkhin <unicon2000@gmail.com>
2020-11-23 21:47:16 +0200

    Signed-off-by: PavelAlkhin <unicon2000@gmail.com>

--- 2020-08-otus-spring-alkhin/spring-02/src/main/java/ru/otus/spring/Main.java
+++ 2020-08-otus-spring-alkhin/spring-02/src/main/java/ru/otus/spring/Main.java
@@ -4,7 +4,6 @@ import com.github.cloudyrock.spring.v5.EnableMongock;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,35 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SpringApplication.run(Main.class, args);
 
-        ApplicationContext ctx = SpringApplication.run(Main.class, args);
-
-//        UserServiceImpl userService = ctx.getBean("UserServiceImpl", UserServiceImpl.class);
-
-
-//        userService.saveUser(new User("user","pass"));
-
-//        userUser.setPassword("pass");
-////        userServiceImpl.saveUser(userUser);
-////
-//        val userAdmin = new User("admin");
-//        userAdmin.setPassword("pass");
-////        userServiceImpl.saveUser(userAdmin);
-//
-//        User userFromDB = userRep.findByUsername(userUser.getUsername());
-//
-//        if (userFromDB != null) {
-//            return;
-//        }
-//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-//
-//        userUser.setRoles(Collections.singleton(new Role("ROLE_USER")));
-////        userUser.setPassword(bCryptPasswordEncoder.encode(userUser.getPassword()));
-//        userUser.setPassword("pass");
-//        userRep.save(userUser);
-//
-//        userAdmin.setRoles(Collections.singleton(new Role("ROLE_ADMIN")));
-//        userAdmin.setPassword(bCryptPasswordEncoder.encode(userAdmin.getPassword()));
-//        userRep.save(userAdmin);
     }
 }
